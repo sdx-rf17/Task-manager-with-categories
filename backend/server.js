7e2b27c9819f64ffeb9d74398093bebabf549f7d
hotdeth <radhiox4@icloud.com>
2025-10-03 18:52:38 +0300

    update Add method

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const taskRoutes = require('./routes/taskRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
 app.use(cors());
